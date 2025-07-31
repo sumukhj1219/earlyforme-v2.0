9038197c2169c7f50978bb169790800dc6a0c37e
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-07-31 19:37:48 +0530

    added open graph image

--- src/components/common/meta/meta.tsx
+++ src/components/common/meta/meta.tsx
@@ -1,165 +1,165 @@
 import { env } from '~/env';
 
 export interface PageMeta {
-    title: string;
-    description: string;
-    keywords?: string[];
-    ogImage?: string;
-    twitterCard?: 'summary' | 'summary_large_image';
+  title: string;
+  description: string;
+  keywords?: string[];
+  ogImage?: string;
+  twitterCard?: 'summary' | 'summary_large_image';
 }
 
 export const siteConfig = {
-    name: 'Earlyfor.me',
-    title: 'Build your waitlists within few clicks.',
-    description: 'The Fastest Way to Build and Launch Waitlists.',
-    url: env.NEXT_PUBLIC_URL || "http://localhost:3000",
-    ogImage: '/meta/og.png',
-    author: {
-        name: 'Sumukh',
-        twitter: '@iam_enginner',
-        github: 'sumukhj1219',
-        email: 'sumukhjoshi75@gmail.com'
-    },
-    keywords: [
-        'multi-tenant',
-        'saas',
-        'waitlists',
-        'developer',
-        'full-stack',
-        'react',
-        'nextjs',
-        'typescript',
-        'webdevelopment',
-    ],
+  name: 'Earlyfor.me',
+  title: 'Build your waitlists within few clicks.',
+  description: 'The Fastest Way to Build and Launch Waitlists.',
+  url: env.NEXT_PUBLIC_URL || "http://localhost:3000",
+  ogImage: '/meta/og.png',
+  author: {
+    name: 'Sumukh',
+    twitter: '@iam_enginner',
+    github: 'sumukhj1219',
+    email: 'sumukhjoshi75@gmail.com'
+  },
+  keywords: [
+    'multi-tenant',
+    'saas',
+    'waitlists',
+    'developer',
+    'full-stack',
+    'react',
+    'nextjs',
+    'typescript',
+    'webdevelopment',
+  ],
 };
 
 export const pageMetadata: Record<string, PageMeta> = {
-    "/": {
-        title: 'Earlyfor.me - Build your waitlists within few clicks.',
-        description: 'The Fastest Way to Build and Launch Waitlists.',
-        keywords: [
-            'Home',
-            'multi-tenant',
-            'saas',
-            'waitlists',
-            'developer',
-            'full-stack',
-            'react',
-            'nextjs',
-            'typescript',
-            'webdevelopment',
-        ],
-        ogImage: '/meta/og.png',
-        twitterCard: 'summary_large_image'
-    },
+  "/": {
+    title: 'Earlyfor.me - Build your waitlists within few clicks.',
+    description: 'The Fastest Way to Build and Launch Waitlists.',
+    keywords: [
+      'Home',
+      'multi-tenant',
+      'saas',
+      'waitlists',
+      'developer',
+      'full-stack',
+      'react',
+      'nextjs',
+      'typescript',
+      'webdevelopment',
+    ],
+    ogImage: '/meta/og.png',
+    twitterCard: 'summary_large_image'
+  },
 
-    '/create': {
-        title: 'Create - Explore templates and create your waitlists.',
-        description: 'With the help of highly customizable templates create your waitlists. Publish them to internet without any domain configuration.',
-        keywords: [
-            'Create waitlists',
-            'Publish',
-            'multi-tenant',
-            'saas',
-            'waitlists',
-            'developer',
-            'full-stack',
-            'react',
-            'nextjs',
-            'typescript',
-            'webdevelopment',
-        ],
-        ogImage: '/meta/logo.png',
-        twitterCard: 'summary'
-    },
+  '/create': {
+    title: 'Create - Explore templates and create your waitlists.',
+    description: 'With the help of highly customizable templates create your waitlists. Publish them to internet without any domain configuration.',
+    keywords: [
+      'Create waitlists',
+      'Publish',
+      'multi-tenant',
+      'saas',
+      'waitlists',
+      'developer',
+      'full-stack',
+      'react',
+      'nextjs',
+      'typescript',
+      'webdevelopment',
+    ],
+    ogImage: '/meta/logo.png',
+    twitterCard: 'summary'
+  },
 
-    '/assets': {
-        title: 'Assets - Manage your logos and images in here to retrive them in future.',
-        description: 'Earlyfor.me has an asset management system helps to manage and retrive assets.',
-        keywords: [
-            'Manage waitlists',
-            'Assets',
-            'Cloudflare',
-            'R2-Object-Storage',
-            'multi-tenant',
-            'saas',
-            'waitlists',
-            'developer',
-            'full-stack',
-            'react',
-            'nextjs',
-            'typescript',
-            'webdevelopment',
-        ],
-        ogImage: '/meta/logo.png',
-        twitterCard: 'summary'
-    },
+  '/assets': {
+    title: 'Assets - Manage your logos and images in here to retrive them in future.',
+    description: 'Earlyfor.me has an asset management system helps to manage and retrive assets.',
+    keywords: [
+      'Manage waitlists',
+      'Assets',
+      'Cloudflare',
+      'R2-Object-Storage',
+      'multi-tenant',
+      'saas',
+      'waitlists',
+      'developer',
+      'full-stack',
+      'react',
+      'nextjs',
+      'typescript',
+      'webdevelopment',
+    ],
+    ogImage: '/meta/logo.png',
+    twitterCard: 'summary'
+  },
 
-    '/waitlists': {
-        title: 'Waitlists - Manage your waitlists and their status',
-        description: 'Earlyfor.me has an waitlist management system helps to manage waitlists.',
-        keywords: [
-            'Manage waitlists',
-            'Waitlists',
-            'Assets',
-            'Cloudflare',
-            'R2-Object-Storage',
-            'multi-tenant',
-            'saas',
-            'waitlists',
-            'developer',
-            'full-stack',
-            'react',
-            'nextjs',
-            'typescript',
-            'webdevelopment',
-        ],
-        ogImage: '/meta/logo.png',
-        twitterCard: 'summary'
-    },
+  '/waitlists': {
+    title: 'Waitlists - Manage your waitlists and their status',
+    description: 'Earlyfor.me has an waitlist management system helps to manage waitlists.',
+    keywords: [
+      'Manage waitlists',
+      'Waitlists',
+      'Assets',
+      'Cloudflare',
+      'R2-Object-Storage',
+      'multi-tenant',
+      'saas',
+      'waitlists',
+      'developer',
+      'full-stack',
+      'react',
+      'nextjs',
+      'typescript',
+      'webdevelopment',
+    ],
+    ogImage: '/meta/logo.png',
+    twitterCard: 'summary'
+  },
 
-    '/analyze/*': {
-        title: 'Analytics - Analyze your customers impressions on your waitlists.',
-        description: 'With enabled analytics it becomes convinient to analyze waitlists and manage them.',
-        keywords: [
-            'Analyze waitlists',
-            'Impressions',
-            'Views',
-            'Subscribers',
-            'R2-Object-Storage',
-            'multi-tenant',
-            'saas',
-            'waitlists',
-            'developer',
-            'full-stack',
-            'react',
-            'nextjs',
-            'typescript',
-            'webdevelopment',
-        ],
-        ogImage: '/meta/logo.png',
-        twitterCard: 'summary'
-    },
+  '/analyze/*': {
+    title: 'Analytics - Analyze your customers impressions on your waitlists.',
+    description: 'With enabled analytics it becomes convinient to analyze waitlists and manage them.',
+    keywords: [
+      'Analyze waitlists',
+      'Impressions',
+      'Views',
+      'Subscribers',
+      'R2-Object-Storage',
+      'multi-tenant',
+      'saas',
+      'waitlists',
+      'developer',
+      'full-stack',
+      'react',
+      'nextjs',
+      'typescript',
+      'webdevelopment',
+    ],
+    ogImage: '/meta/logo.png',
+    twitterCard: 'summary'
+  },
 
-    '/edit/*': {
-        title: 'Edit - Edit your waitlists.',
-        description: 'Earlyfor.me enables you to customize your waitlists anytime and anywhere.',
-        keywords: [
-            'Edit waitlists',
-            'R2-Object-Storage',
-            'multi-tenant',
-            'saas',
-            'waitlists',
-            'developer',
-            'full-stack',
-            'react',
-            'nextjs',
-            'typescript',
-            'webdevelopment',
-        ],
-        ogImage: '/meta/logo.png',
-        twitterCard: 'summary'
-    }
+  '/edit/*': {
+    title: 'Edit - Edit your waitlists.',
+    description: 'Earlyfor.me enables you to customize your waitlists anytime and anywhere.',
+    keywords: [
+      'Edit waitlists',
+      'R2-Object-Storage',
+      'multi-tenant',
+      'saas',
+      'waitlists',
+      'developer',
+      'full-stack',
+      'react',
+      'nextjs',
+      'typescript',
+      'webdevelopment',
+    ],
+    ogImage: '/meta/logo.png',
+    twitterCard: 'summary'
+  }
 }
 
 export function getPageMetadata(pathname: string): PageMeta {
@@ -177,6 +177,8 @@ export function getPageMetadata(pathname: string): PageMeta {
 
 export function generateMetadata(pathname: string) {
   const pageMeta = getPageMetadata(pathname)
+  const imageUrl = new URL(pageMeta.ogImage || siteConfig.ogImage, siteConfig.url).toString();
+
   return {
     metadataBase: new URL(siteConfig.url),
     title: pageMeta.title,
@@ -192,7 +194,7 @@ export function generateMetadata(pathname: string) {
       siteName: siteConfig.title,
       images: [
         {
-          url: pageMeta.ogImage || siteConfig.ogImage,
+          url: imageUrl,
           width: 1200,
           height: 630,
           alt: pageMeta.title,
@@ -204,7 +206,7 @@ export function generateMetadata(pathname: string) {
       title: pageMeta.title,
       description: pageMeta.description,
       creator: siteConfig.author.twitter,
-      images: [pageMeta.ogImage || siteConfig.ogImage],
+      images: [imageUrl],
     },
     robots: {
       index: true,
@@ -221,5 +223,6 @@ export function generateMetadata(pathname: string) {
       canonical: `${siteConfig.url}${pathname}`,
     },
   }
+
 }
 
