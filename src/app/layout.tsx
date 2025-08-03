a222e8d32cc8a9f034d395a03c394906586d8be5
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-08-03 12:22:31 +0530

    fixing meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,66 @@
 import "~/styles/globals.css";
 
-import { type Metadata } from "next";
 import { Geist } from "next/font/google";
-import { SessionProvider } from "next-auth/react"
+import { type Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
 import { TRPCReactProvider } from "~/trpc/react";
 import { ThemeSwitchProvider } from "~/components/common/providers/theme-provider";
 import { WaitlistContextProvider } from "~/contexts/WaitlistContext";
 import { Toaster } from "sonner";
-import { generateMetadata } from "~/components/common/meta/meta";
-import Head from "next/head";
-
-
-export const metadata = generateMetadata("/")
 
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
 });
-<Head>
-  <meta property="og:image" content="https://earlyfor.me/meta/main.png" />
-  <meta name="twitter:image" content="https://earlyfor.me/meta/main.png" />
-  <meta name="twitter:card" content="summary_large_image" />
-</Head>
+
+export const metadata: Metadata = {
+  title: "Earlyfor.me - Build your waitlists within few clicks.",
+  description: "The Fastest Way to Build and Launch Waitlists.",
+  keywords: [
+    "multi-tenant",
+    "saas",
+    "waitlists",
+    "developer",
+    "full-stack",
+    "react",
+    "nextjs",
+    "typescript",
+    "webdevelopment",
+  ],
+  metadataBase: new URL("https://earlyfor.me"), 
+  openGraph: {
+    title: "Earlyfor.me - Build your waitlists within few clicks.",
+    description: "The Fastest Way to Build and Launch Waitlists.",
+    url: "https://earlyfor.me",
+    siteName: "Earlyfor.me",
+    images: [
+      {
+        url: "https://earlyfor.me/meta/main.png", 
+        width: 1200,
+        height: 630,
+        alt: "Earlyfor.me",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Earlyfor.me - Build your waitlists within few clicks.",
+    description: "The Fastest Way to Build and Launch Waitlists.",
+    creator: "@iam_enginner",
+    images: ["https://earlyfor.me/meta/main.png"],
+  },
+  alternates: {
+    canonical: "https://earlyfor.me",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body className=" selection:bg-orange-600">
+      <body className="selection:bg-orange-600">
         <SessionProvider>
           <ThemeSwitchProvider>
             <WaitlistContextProvider>
@@ -39,7 +71,6 @@ export default function RootLayout({
             </WaitlistContextProvider>
           </ThemeSwitchProvider>
         </SessionProvider>
-
       </body>
     </html>
   );
